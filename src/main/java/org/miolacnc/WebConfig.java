8d7ec276ca4f1f5b1ccdd35f9e2f0a14374431ca
Meriem-Hammouchi <50526643+Meriem-Hammouchi@users.noreply.github.com>
2020-11-19 22:47:29 +0100

    Update WebConfig.java

--- src/main/java/org/miolacnc/WebConfig.java
+++ src/main/java/org/miolacnc/WebConfig.java
@@ -10,9 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry
 		        .addMapping("/**")
-		        .allowedMethods("*")
-		        .allowedOrigins("*")
-		        .allowedHeaders("*");
+		        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+		        .allowedOrigins("https://app-miolaconcours-student.herokuapp.com")
+		        .allowedHeaders("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+                "Cache-Control", "Content-Type");
 		       
 	}	
 
