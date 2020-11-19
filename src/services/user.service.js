9dcbd0f011b6adb9c616f10cf6a47dde7966307f
Meriem-Hammouchi <50526643+Meriem-Hammouchi@users.noreply.github.com>
2020-11-19 15:05:43 +0100

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8181/api/test/";
+const API_URL = "http://miolaconcours-student-app.herokuapp.com/api/test/";
 
 class UserService {
   getPublicContent() {
